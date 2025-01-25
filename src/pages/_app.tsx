6c00b0b031b7e3df2599b2aa3ba810b60b173a3c
adonisamarante <adonisamarante@gmail.com>
2025-01-25 14:48:01 -0300

    feat: header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+
 // every time something happen, App will re-render, so it is recommended to
 // leave globalStyles() out of the App function because it's values don't change
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
