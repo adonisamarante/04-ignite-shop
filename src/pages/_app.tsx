b8e3519b56684666a35914be09419180f89027ae
adonisamarante <adonisamarante@gmail.com>
2025-01-25 14:33:43 -0300

    feat: global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,9 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+// every time something happen, App will re-render, so it is recommended to
+// leave globalStyles() out of the App function because it's values don't change
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
