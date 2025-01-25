e11df508ae176aaf5ffe2205713be904f2d80e16
adonisamarante <adonisamarante@gmail.com>
2025-01-25 14:59:24 -0300

    refactor: using Image component from nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { globalStyles } from '../styles/global'
 
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
 
 // every time something happen, App will re-render, so it is recommended to
 // leave globalStyles() out of the App function because it's values don't change
@@ -12,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
