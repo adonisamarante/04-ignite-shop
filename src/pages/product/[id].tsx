7f21c5709eab6c3a0290655c24c0001fd3e4e6bb
adonisamarante <adonisamarante@gmail.com>
2025-01-31 15:51:03 -0300

    feat: product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,27 @@
-import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/src/styles/pages/product'
 
 export default function Product() {
-  const { query } = useRouter()
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
 
-  return <h1>Product: {query.id}</h1>
+      <ProductDetails>
+        <h1>Camiseta</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Reprehenderit pariatur nam labore! Repellat rem fugiat reiciendis
+          perferendis voluptatum illo iusto praesentium, magnam eaque recusandae
+          cupiditate accusamus expedita molestiae sed vitae.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
