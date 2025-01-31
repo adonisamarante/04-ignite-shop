a67e67535b3fa01a7bbec481f246bf9d04d1a5ef
adonisamarante <adonisamarante@gmail.com>
2025-01-31 16:22:40 -0300

    feat: redirect to home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,27 @@ import { globalStyles } from '../styles/global'
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 // every time something happen, App will re-render, so it is recommended to
 // leave globalStyles() out of the App function because it's values don't change
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const isHomePage = router.pathname === '/'
+
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        {!isHomePage ? (
+          <Link href="/">
+            <Image src={logoImg} alt="" />
+          </Link>
+        ) : (
+          <Image src={logoImg} alt="" />
+        )}
       </Header>
 
       <Component {...pageProps} />
