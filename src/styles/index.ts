829667ec38d83d15e6c861ef5f8a7d451869e544
adonisamarante <adonisamarante@gmail.com>
2025-02-24 16:23:19 -0300

    feat: colors names according to figma

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -14,21 +14,21 @@ export const {
     colors: {
       white: '#FFF',
 
-      gray900: '#121214',
-      gray800: '#202024',
-      gray500: '#8D8D99',
-      gray300: '#C4C4CC',
-      gray100: '#E1E1E6',
+      gray900: '#121214', // background
+      gray800: '#202024', // elements
+      gray500: '#8D8D99', // icon
+      gray300: '#C4C4CC', // text
+      gray100: '#E1E1E6', // title
 
-      green500: '#00875F',
-      green300: '#00B37E',
+      green500: '#00875F', // principal
+      green300: '#00B37E', // light
     },
 
     fontSizes: {
-      md: '1,125rem',
-      lg: '1,25rem',
-      xl: '1,5rem',
-      '2xl': '2rem',
+      md: '1,125rem', // 18
+      lg: '1,25rem', // 20
+      xl: '1,5rem', // 24
+      '2xl': '2rem', // 32
     },
   },
 })
